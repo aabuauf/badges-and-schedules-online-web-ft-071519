afd22df3291120d6ce500ec6c89b2c27efe95408
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-15 13:06:56 +0000

    Automatically backed up by Learn

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -54,8 +54,8 @@ TEXT
   describe '#badge_maker' do
 
     # Question 1
-def badge_maker()
-  return 
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
     it 'should return a formatted badge' do
       expect(badge_maker(name)).to eq("Hello, my name is #{name}.")
